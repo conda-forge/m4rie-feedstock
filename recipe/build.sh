42af976d74900f6aafddef4715970ca1e89f93b7
Isuru Fernando <isuruf@gmail.com>
2019-10-30 15:44:31 -0500

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,13 +3,9 @@
 export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${PREFIX}/lib"
 export CFLAGS="-O2 -g -fPIC $CFLAGS -L${PREFIX}/lib"
 
-# Get rid of any `.la` from defaults.
-find $PREFIX/lib -name '*.la' -delete
-
-
 chmod +x configure
 
-./configure --prefix=$PREFIX --libdir=$PREFIX/lib --disable-sse2
+./configure --prefix=$PREFIX --libdir=$PREFIX/lib
 
 make
 make check
